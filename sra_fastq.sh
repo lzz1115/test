18d425f0e7cf0433a62248f1cb20f5eba2c553f7
lzz1115 <148315890+lzz1115@users.noreply.github.com>
2023-10-19 14:11:41 +0800

    Update sra_fastq.sh

--- sra_fastq.sh
+++ sra_fastq.sh
@@ -1,3 +1,4 @@
+
 #! /bin/bash
 
 ###list all sra files
@@ -5,3 +6,4 @@ for i in *sra
 do
 echo fastq-dump --gzip --split-3 $i -O /cluster/home/luzhenzhen/batch/download/sra/fastq ## -O output files address
 done >sra_fastq.sh
+
