d6179de5f9018ccfb78a5a846b4826ef2dd714e1
lzz1115 <148315890+lzz1115@users.noreply.github.com>
2023-10-31 10:11:42 +0800

    Create 1024-1.sh

---
+++ 1024-1.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+clear
+
+# Setting variables
+method=INDUCE_SEQ
+platform=NEXTSEQ550
+FASTQ=/cluster/home/luzhenzhen/batch/download/FASTQ
+SUM=/cluster/home/luzhenzhen/batch/download/summary
+blacklist=/cluster/home/luzhenzhen/batch/accessory_files/hg19-blacklist.v2.bed
+chromsizes=/cluster/home/luzhenzhen/batch/accessory_files/hg19.chrom.sizes.bed
+chromends=/cluster/home/luzhenzhen/batch/accessory_files/hg19.chrom.ends.bed
+refseq=/cluster/home/luzhenzhen/batch/download/index/Homo_sapiens_assembly38.fasta
+quality=30
+threads=16
+
+for file in "$FASTQ"/*_trimmed.fq.gz; do
+filename=$(echo "$file" | awk -F'[/]' '{print $8}')
+experiment=$(echo "$filename" | awk -F'[_]' '{print $1}')
+
+tmpdir=/cluster/groups/Jan-Lab/luzhenzhen/"$experiment"/preprocessing/tmpdir
+BAM=/cluster/groups/Jan-Lab/luzhenzhen/"$experiment"/preprocessing/BAM
+SAM=/cluster/groups/Jan-Lab/luzhenzhen/"$experiment"/preprocessing/SAM
+BED=/cluster/groups/Jan-Lab/luzhenzhen/"$experiment"/preprocessing/BEM
+
+awk '$6 !~ /[0-9]S/{print}' "$SAM"/"$experiment".sam | samtools view -Shu -q "$quality" -F 256 - |
+  samtools sort -m 4G - -o "$BAM"/"$experiment".q30.srt.bam
+  samtools index "$BAM"/"$experiment".q30.srt.bam
+
+done
