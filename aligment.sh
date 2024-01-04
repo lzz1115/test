49a89cd30036318769cc8d2df812c8eddf52b296
lzz1115 <148315890+lzz1115@users.noreply.github.com>
2024-01-04 22:47:36 +0800

    Update and rename 1019.sh to aligment.sh

--- aligment.sh
+++ aligment.sh
@@ -3,23 +3,23 @@ clear
 
 # Setting variables
 method=INDUCE_SEQ
-platform=NEXTSEQ550
+platform=NovaSeqXp
 BAM=BAM
 SAM=SAM
 BED=BED
-FASTQ=/cluster/home/luzhenzhen/batch/download/FASTQ
+FASTQ=/cluster/home/luzhenzhen/rowdata/1208result/FASTQ/af_trimmed/FASTQ #修改地址
 SUM=summary
 tmpdir=tmpdir
 blacklist=/cluster/home/luzhenzhen/batch/accessory_files/hg19-blacklist.v2.bed
 chromsizes=/cluster/home/luzhenzhen/batch/accessory_files/hg19.chrom.sizes.bed
 chromends=/cluster/home/luzhenzhen/batch/accessory_files/hg19.chrom.ends.bed
 refseq=/cluster/home/luzhenzhen/batch/download/index/Homo_sapiens_assembly38.fasta
-quality=30
+quality=1 #quality=30
 threads=16
 
 #Detect FASTQ files
-for file in "$FASTQ"/*_trimmed.fq.gz; do (
-filename=$(echo "$file" | awk -F'[/]' '{print $8}')
+for file in "$FASTQ"/*_fq.fq.gz; do (
+filename=$(echo "$file" | awk -F'[/]' '{print $10}')# $几：要根据文件地址修改
 echo "$filename" >> filelist.txt
 experiment=$(echo "$filename" | awk -F'[_]' '{print $1}')
 echo "Found" "$filename" "from experiment:" "$experiment"
@@ -39,14 +39,17 @@ mkdir -p /cluster/groups/Jan-Lab/luzhenzhen/"$experiment"/preprocessing/"$tmpdir
 tmpdir=/cluster/groups/Jan-Lab/luzhenzhen/"$experiment"/preprocessing/"$tmpdir"
 mkdir -p /cluster/home/luzhenzhen/batch/download/summary
 
-echo "$SAM" "$experiment" "$threads" "$method" "$tmpdir" "$platform" "$refseq"  "$filename"
-
 # ALIGNMENT
 if [[ ! -e "$SAM"/"$experiment".sam && ! -e "$tmpdir"/"$experiment".sam ]]; then
-  bwa mem -M -p -R '@RG\tID:"$method"\tPL:"$platform"\tPU:0\tLB:"$method"\tSM:"$experiment"' "$refseq" "$FASTQ"/"$filename" > "$tmpdir"/"$experiment".sam
+  bwa mem -M -R '@RG\tID:"$method"\tPL:"$platform"\tPU:0\tLB:"$method"\tSM:"$experiment"' "$refseq" "$FASTQ"/"$experiment"_R1_val_1.fq.gz "$FASTQ"/"$experiment"_R2_val_2.fq.gz > "$tmpdir"/"$experiment".sam
+
+awk '$6 !~ /[0-9]S/{print}' "$tmpdir"/"$experiment".sam | samtools view -Shu -q "$quality" -F 256 - |
+  samtools sort -m 4G - -o "$BAM"/"$experiment".q1.srt.bam
+  samtools index "$BAM"/"$experiment".q1.srt.bam
+  mv "$tmpdir"/"$experiment"_1.sam "$SAM"/"$experiment"_1.sam
 
 else
-  echo "SAM file found in:" "$tmpdir"/"$experiment".sam "Alignment in progress, skipping."
+  echo "SAM file found in:" "$tmpdir"/"$experiment"_1.sam "Alignment in progress, skipping."
 fi
 )
 done
