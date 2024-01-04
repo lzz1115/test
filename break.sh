d95941fd5888840bfec987807837df88d280df3a
lzz1115 <148315890+lzz1115@users.noreply.github.com>
2024-01-04 23:31:24 +0800

    Update break.sh

--- break.sh
+++ break.sh
@@ -13,7 +13,7 @@ refseq=/cluster/home/luzhenzhen/batch/download/index/Homo_sapiens_assembly38.fas
 quality=1 #quality=30
 threads=16
 
-for file in "$FASTQ"/*.fq.gz; do
+for file in "$FASTQ"/*fq.gz; do
 filename=$(echo "$file" | awk -F'[/]' '{print $10}') #按地址修改数值
 experiment=$(echo "$filename" | awk -F'[_]' '{print $1}')
 
